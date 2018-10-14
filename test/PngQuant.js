2a0d19339dadf8707a64e681e35b5491de85072a
Andreas Lind <andreaslindpetersen@gmail.com>
2018-10-14 12:55:38 +0200

    for transform in arrow arrow-return let obj-shorthand obj-method ; do for dir in lib test; do lebab --transform $transform --replace $dir; done ; done

--- test/PngQuant.js
+++ test/PngQuant.js
@@ -1,13 +1,13 @@
 /*global describe, it, beforeEach, afterEach, setTimeout, __dirname*/
-var expect = require('unexpected')
+const expect = require('unexpected')
   .clone()
   .use(require('unexpected-stream'))
   .use(require('unexpected-sinon'));
-var sinon = require('sinon');
-var PngQuant = require('../lib/PngQuant');
-var pathModule = require('path');
-var fs = require('fs');
-var semver = require('semver');
+const sinon = require('sinon');
+const PngQuant = require('../lib/PngQuant');
+const pathModule = require('path');
+const fs = require('fs');
+const semver = require('semver');
 
 it.skipIf = function(condition) {
   (condition ? it.skip : it).apply(
@@ -16,26 +16,24 @@ it.skipIf = function(condition) {
   );
 };
 
-describe('PngQuant', function() {
-  it('should produce a smaller file with lower quality and ordered dithering', function() {
-    return expect(
-      fs.createReadStream(
-        pathModule.resolve(__dirname, 'purplealpha24bit.png')
-      ),
-      'when piped through',
-      new PngQuant([128, '--quality', '60-80', '--nofs']),
-      'to yield output satisfying',
-      function(resultPngBuffer) {
-        expect(resultPngBuffer.length, 'to be within', 0, 8285);
-      }
-    );
-  });
+describe('PngQuant', () => {
+  it('should produce a smaller file with lower quality and ordered dithering', () => expect(
+    fs.createReadStream(
+      pathModule.resolve(__dirname, 'purplealpha24bit.png')
+    ),
+    'when piped through',
+    new PngQuant([128, '--quality', '60-80', '--nofs']),
+    'to yield output satisfying',
+    resultPngBuffer => {
+      expect(resultPngBuffer.length, 'to be within', 0, 8285);
+    }
+  ));
 
   it.skipIf(
     semver.satisfies(process.version.replace(/^v/, ''), '>=0.12.0'),
     'should not emit data events while paused',
-    function(done) {
-      var pngQuant = new PngQuant();
+    done => {
+      const pngQuant = new PngQuant();
 
       function fail() {
         done(new Error('PngQuant emitted data while it was paused!'));
@@ -47,16 +45,16 @@ describe('PngQuant', function() {
         pathModule.resolve(__dirname, 'purplealpha24bit.png')
       ).pipe(pngQuant);
 
-      setTimeout(function() {
+      setTimeout(() => {
         pngQuant.removeListener('data', fail);
-        var chunks = [];
+        const chunks = [];
 
         pngQuant
-          .on('data', function(chunk) {
+          .on('data', chunk => {
             chunks.push(chunk);
           })
-          .on('end', function() {
-            var resultPngBuffer = Buffer.concat(chunks);
+          .on('end', () => {
+            const resultPngBuffer = Buffer.concat(chunks);
             expect(resultPngBuffer.length, 'to be within', 0, 8285);
             done();
           });
@@ -66,30 +64,30 @@ describe('PngQuant', function() {
     }
   );
 
-  it('should emit an error if an invalid image is processed', function(done) {
-    var pngQuant = new PngQuant();
+  it('should emit an error if an invalid image is processed', done => {
+    const pngQuant = new PngQuant();
 
     pngQuant
-      .on('error', function(err) {
+      .on('error', err => {
         done();
       })
-      .on('data', function(chunk) {
+      .on('data', chunk => {
         done(new Error('PngQuant emitted data when an error was expected'));
       })
-      .on('end', function(chunk) {
+      .on('end', chunk => {
         done(new Error('PngQuant emitted end when an error was expected'));
       });
 
     pngQuant.end(new Buffer('qwvopeqwovkqvwiejvq', 'utf-8'));
   });
 
-  it('should emit a single error if an invalid command line is specified', function(done) {
-    var pngQuant = new PngQuant(['--blabla']);
+  it('should emit a single error if an invalid command line is specified', done => {
+    const pngQuant = new PngQuant(['--blabla']);
 
-    var seenError = false;
+    let seenError = false;
 
     pngQuant
-      .on('error', function(err) {
+      .on('error', err => {
         expect(pngQuant.commandLine, 'to match', /pngquant --blabla/);
         if (seenError) {
           done(new Error('More than one error event was emitted'));
@@ -98,25 +96,25 @@ describe('PngQuant', function() {
           setTimeout(done, 100);
         }
       })
-      .on('data', function(chunk) {
+      .on('data', chunk => {
         done(new Error('PngQuant emitted data when an error was expected'));
       })
-      .on('end', function(chunk) {
+      .on('end', chunk => {
         done(new Error('PngQuant emitted end when an error was expected'));
       });
 
     pngQuant.end(new Buffer('qwvopeqwovkqvwiejvq', 'utf-8'));
   });
 
-  describe('#destroy', function() {
-    it('should kill the underlying child process', function() {
-      var pngQuant = new PngQuant(['-grayscale']);
+  describe('#destroy', () => {
+    it('should kill the underlying child process', () => {
+      const pngQuant = new PngQuant(['-grayscale']);
 
-      return expect.promise(function(run) {
+      return expect.promise(run => {
         pngQuant.write('JFIF');
         setTimeout(
           run(function waitForPngQuantProcess() {
-            var pngQuantProcess = pngQuant.pngQuantProcess;
+            const pngQuantProcess = pngQuant.pngQuantProcess;
             if (pngQuant.pngQuantProcess) {
               sinon.spy(pngQuantProcess, 'kill');
               pngQuant.destroy();
@@ -124,14 +122,14 @@ describe('PngQuant', function() {
               expect(
                 pngQuantProcess.kill,
                 'to have calls satisfying',
-                function() {
+                () => {
                   pngQuantProcess.kill();
                 }
               );
               expect(pngQuant.pngQuantProcess, 'to be falsy');
               expect(pngQuant.bufferedChunks, 'to be falsy');
               setTimeout(
-                run(function() {
+                run(() => {
                   expect(pngQuant.emit, 'to have calls satisfying', []);
                 }),
                 10
@@ -146,50 +144,38 @@ describe('PngQuant', function() {
     });
   });
 
-  expect.addAssertion('<Stream> to error', function(expect, subject) {
-    return expect.promise(function(run) {
-      subject.once(
-        'error',
-        run(function(err) {
-          return err;
-        })
-      );
-    });
-  });
+  expect.addAssertion('<Stream> to error', (expect, subject) => expect.promise(run => {
+    subject.once(
+      'error',
+      run(err => err)
+    );
+  }));
 
-  expect.addAssertion('<Stream> to error with <any>', function(
-    expect,
-    subject,
-    value
-  ) {
+  expect.addAssertion('<Stream> to error with <any>', (expect, subject, value) => {
     expect.errorMode = 'nested';
-    return expect(subject, 'to error').then(function(err) {
-      return expect(err, 'to satisfy', value);
-    });
+    return expect(subject, 'to error').then(err => expect(err, 'to satisfy', value));
   });
 
-  describe('with an overridden #binaryPath', function() {
-    var childProcess = require('child_process');
-    beforeEach(function() {
+  describe('with an overridden #binaryPath', () => {
+    const childProcess = require('child_process');
+    beforeEach(() => {
       PngQuant.getBinaryPath.purgeAll(); // Make sure we don't get a cached value from one of the other tests
       PngQuant.binaryPath = '/foo/bar';
       sinon.spy(childProcess, 'spawn');
     });
-    afterEach(function() {
+    afterEach(() => {
       PngQuant.binaryPath = null;
       childProcess.spawn.restore();
     });
 
-    it('should try launching that binary instead of the one found on the system', function() {
-      return expect(
-        fs.createReadStream(
-          pathModule.resolve(__dirname, 'purplealpha24bit.png')
-        ),
-        'when piped through',
-        new PngQuant([128]),
-        'to error with',
-        /\/foo\/bar ENOENT|write EPIPE/
-      );
-    });
+    it('should try launching that binary instead of the one found on the system', () => expect(
+      fs.createReadStream(
+        pathModule.resolve(__dirname, 'purplealpha24bit.png')
+      ),
+      'when piped through',
+      new PngQuant([128]),
+      'to error with',
+      /\/foo\/bar ENOENT|write EPIPE/
+    ));
   });
 });
